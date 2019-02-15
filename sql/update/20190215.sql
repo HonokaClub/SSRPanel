a9bc4a305078d7427cc292ce4a689ed5e5a9d506
ZaneQ <zhangqian2002@outlook.com>
2019-02-15 22:41:32 +0800

    add alipayf2f to sql

--- sql/update/20190215.sql
+++ sql/update/20190215.sql
@@ -1,3 +1,8 @@
 -- 邮件投递记录增加最后更新字段
 ALTER TABLE `email_log`
 	ADD COLUMN `updated_at` DATETIME NULL DEFAULT NULL COMMENT '最后更新时间' AFTER `created_at`;
+
+INSERT INTO `config` values ('87', 'is_f2fpay', 0);
+INSERT INTO `config` VALUES ('88', 'f2fpay_app_id', '');
+INSERT INTO `config` VALUES ('89', 'f2fpay_private_key', '');
+INSERT INTO `config` VALUES ('90', 'f2fpay_public_key', '');
