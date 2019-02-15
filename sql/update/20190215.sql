e70d09bd744089c2c42fa8b034679abe41976734
ZaneQ <zhangqian2002@outlook.com>
2019-02-15 22:58:55 +0800

    Update 20190215.sql

--- sql/update/20190215.sql
+++ sql/update/20190215.sql
@@ -2,7 +2,7 @@
 ALTER TABLE `email_log`
 	ADD COLUMN `updated_at` DATETIME NULL DEFAULT NULL COMMENT '最后更新时间' AFTER `created_at`;
 
-INSERT INTO `config` values ('87', 'is_f2fpay', 0);
-INSERT INTO `config` VALUES ('88', 'f2fpay_app_id', '');
-INSERT INTO `config` VALUES ('89', 'f2fpay_private_key', '');
-INSERT INTO `config` VALUES ('90', 'f2fpay_public_key', '');
+INSERT INTO `config` values ('90', 'is_f2fpay', 0);
+INSERT INTO `config` VALUES ('91', 'f2fpay_app_id', '');
+INSERT INTO `config` VALUES ('92', 'f2fpay_private_key', '');
+INSERT INTO `config` VALUES ('93', 'f2fpay_public_key', '');
